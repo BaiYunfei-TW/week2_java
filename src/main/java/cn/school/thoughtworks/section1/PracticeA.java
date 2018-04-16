cd8b569b0e37c64265ae9aafaacc480e5c91a6cc
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-04-16 12:46:01 +0800

    PracticeA success

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,19 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result = new LinkedList<>();
+
+        HashSet<String> existSet = new HashSet<>(collection2);  //记录collection2中的元素
+        for (String s : collection1) {  //遍历collection1
+            if (existSet.contains(s)){  //如果在collection2中有，则加到结果计中
+                result.add(s);
+            }
+        }
+        return result;
     }
 }
