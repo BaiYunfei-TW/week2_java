571110f38b0dd112bbf8b6068a97623427d5add2
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-04-23 09:59:59 +0800

    practice B and C passed

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,22 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result = new LinkedList<>();
+
+        HashSet<String> c2ElementSet = new HashSet<>();
+        for(List<String> childList: collection2){
+            c2ElementSet.addAll(childList);
+        }
+        for(String str: collection1){
+            if(c2ElementSet.contains(str)){
+                result.add(str);
+            }
+        }
+        return result;
     }
 }
